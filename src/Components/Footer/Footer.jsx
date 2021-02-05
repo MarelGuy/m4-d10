5a1c95e6ddb8577020ea40d985ecb0c434f940c8
ArturSmiles <72197892+ArturSmiles@users.noreply.github.com>
2021-02-05 15:51:15 +0100

    Added alt to picture

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import AudioPlayer,{ RHAP_UI } from 'react-h5-audio-player';
-import{Row,Col} from "react-bootstrap"
 import 'react-h5-audio-player/lib/styles.css';
 import "./Footer.css"
 import { BsFillSkipStartFill,BsFillSkipEndFill,BsPlayFill } from 'react-icons/bs'
@@ -12,7 +11,7 @@ function Footer() {
           src="https://cdns-preview-c.dzcdn.net/stream/c-cca63b2c92773d54e61c5b4d17695bd2-8.mp3"
           customProgressBarSection={
             [
-                <img src="https://cdns-images.dzcdn.net/images/cover/ec3c8ed67427064c70f67e5815b74cef/56x56-000000-80-0-0.jpg"/>,
+                <img src="https://cdns-images.dzcdn.net/images/cover/ec3c8ed67427064c70f67e5815b74cef/56x56-000000-80-0-0.jpg" alt="Album"/>,
                 <p style={{marginRight:"20px", marginLeft:"5px"}}>Eminem Without Me</p>,
               RHAP_UI.CURRENT_TIME,
               <div>/</div>,
