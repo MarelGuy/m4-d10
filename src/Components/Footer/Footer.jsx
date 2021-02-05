48333ca0af387ac02eff9c2b90fb8c5ccebb0589
ArturSmiles <72197892+ArturSmiles@users.noreply.github.com>
2021-02-05 14:13:59 +0100

    Added Player

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,49 +1,37 @@
 import React from 'react'
+import AudioPlayer,{ RHAP_UI } from 'react-h5-audio-player';
+import{Row,Col} from "react-bootstrap"
+import 'react-h5-audio-player/lib/styles.css';
+import "./Footer.css"
+import { BsFillSkipStartFill,BsFillSkipEndFill,BsPlayFill } from 'react-icons/bs'
 
 function Footer() {
     return (
         <>
-            <div id="footer" className="row fixed-bottom justify-content-bewteen align-items-center py-2 px-3 bg-dark text-white">
-                <div className="col col-md-3 d-flex align-items-center justify-content-start">
-                    <img src="" className="img-fluid mr-1" alt="no img here, go away" />
-                    <div className="d-flex flex-column justify-content-around mx-2">
-                        <label className="current-title m-0">Idk put a song here</label>
-                        <label className="current-artist m-0 text-muted">Yikes</label>
-                    </div>
-                    <i className="fa fa-heart-o mx-2" aria-hidden="true"></i>
-                    <i className="fa fa-picture-o mx-2" aria-hidden="true"></i>
-                </div>
-                <div className="col col-md-6 px-5">
-                    <div id="controls" className="d-flex justify-content-around align-items-center px-5 py-1">
-                        <i className="fa fa-random fa-sm mr-2" aria-hidden="true"></i>
-                        <i className="fa fa-fast-backward mr-2" aria-hidden="true"></i>
-                        <i className="fa fa-play-circle-o fa-lg" aria-hidden="true"></i>
-                        <i className="fa fa-fast-forward mr-2" aria-hidden="true"></i>
-                        <i className="fa fa-refresh mr-2" aria-hidden="true"></i>
-                    </div>
-                    <div id="progress-time" className="d-flex align-items-center">
-                        <div id="current-time" className="mr-2">oh:oh</div>
-                        <div className="progress w-100">
-                            <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
-                                aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-
-                        <div id="album-duration" className="ml-2">mukatte:kuruno:ka</div>
-                    </div>
-                </div>
-                <div className="col col-md-3 d-flex justify-content-end align-items-center">
-                    <i className="fa fa-list mx-2" aria-hidden="true"></i>
-                    <div className="mx-2 d-flex justify-content-center align-items-center">
-                        <i className="fa fa-window-maximize" aria-hidden="true"></i>
-                        <i className="fa fa-mobile fa-lg" aria-hidden="true"></i>
-                    </div>
-                    <i className="fa fa-volume-up mx-2" aria-hidden="true"></i>
-                    <div className="progress w-25">
-                        <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
-                            aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
+            <AudioPlayer
+          src="https://cdns-preview-c.dzcdn.net/stream/c-cca63b2c92773d54e61c5b4d17695bd2-8.mp3"
+          customProgressBarSection={
+            [
+                <img src="https://cdns-images.dzcdn.net/images/cover/ec3c8ed67427064c70f67e5815b74cef/56x56-000000-80-0-0.jpg"/>,
+                <p style={{marginRight:"20px", marginLeft:"5px"}}>Eminem Without Me</p>,
+              RHAP_UI.CURRENT_TIME,
+              <div>/</div>,
+              RHAP_UI.DURATION,
+              RHAP_UI.PROGRESS_BAR,
+              RHAP_UI.VOLUME,
+            ]}
+            customControlsSection={[RHAP_UI.ADDITIONAL_CONTROLS, RHAP_UI.MAIN_CONTROLS,RHAP_UI.LOOP, RHAP_UI.VOLUME_CONTROLS]}
+            customVolumeControls={[]}
+            customAdditionalControls={[]}
+            layout="stacked-reverse"
+            showSkipControls={true} showJumpControls={false}
+            customIcons={{
+                play:<BsPlayFill/>,
+                previous:<BsFillSkipStartFill/>,
+                next:<BsFillSkipEndFill/>,
+            }
+            }
+          />
         </>
     )
 }
